024d03bb1886a8cbbce9af3cea4e301acfc4820f
Hiroki Noda <kubo39@gmail.com>
2018-04-06 09:59:53 +0900

    Add units

--- src/bin/httpd.rs
+++ src/bin/httpd.rs
@@ -1,5 +1,8 @@
 extern crate jellyfish;
 
+use jellyfish::core::units;
+
 fn main() {
+    const A: usize = units::KB;
     println!("Hello, World!");
 }
---
+++ src/core/units.rs
@@ -0,0 +1,3 @@
+pub const KB: usize = 1 << 10;
+pub const MB: usize = 1 << 20;
+pub const GB: usize = 1 << 30;
